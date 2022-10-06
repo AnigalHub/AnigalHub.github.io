39cafc3eaea1d6e6d99a7c8b636e97a83cdf8778
AnigalHub <anigal1997@gmail.com>
2022-10-06 19:30:39 +0300

    исправлены ошибки по чоп

--- security/_nuxt/static/1664926060/services/_guard_of_real_estate/services/_guard_of_organizations/guard_of_medical_institutions/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsecurity\u002F_nuxt\u002Fstatic\u002F1664926060",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{}}}("\u002Fservices\u002F_guard_of_real_estate\u002Fservices\u002F_guard_of_organizations\u002Fguard_of_medical_institutions"));
