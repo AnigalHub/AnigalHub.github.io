9a29b7c81182fc46c8a4bfc241ccfe36f4022074
AnigalHub <anigal1997@gmail.com>
2024-05-12 00:30:43 +0300

    изменение тем

---
+++ _nuxt/static/1715376514/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1715376514",layout:"default",error:null,state:{Themes:{types:[{name:"Светлая тема",type:a},{name:"Темная тема",type:"dark"}],currentTheme:a,systemTheme:a}},serverRendered:true,routePath:"\u002F",config:{}}}("light"));
