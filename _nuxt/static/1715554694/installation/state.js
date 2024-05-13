cc010aa0ef18694b67c2b173f3d14d91599845f6
AnigalHub <anigal1997@gmail.com>
2024-05-14 02:00:06 +0300

    правка по темам

---
+++ _nuxt/static/1715554694/installation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1715554694",layout:"default",error:{statusCode:404,path:b,message:"This page could not be found"},state:{Themes:{types:[{name:"Светлая тема",type:a},{name:"Темная тема",type:"dark"}],currentTheme:a,systemTheme:a}},serverRendered:false,routePath:b,config:{}}}("light","\u002Finstallation"));
